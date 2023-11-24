571d3212b61b858747de0c84e1f25640e824befc
David Fant <david@fant.io>
2023-11-24 23:00:40 +0000

    updated odoo snapshot restore script

--- halpert/functions/odoo/snapshot/restore.py
+++ halpert/functions/odoo/snapshot/restore.py
@@ -7,17 +7,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+default_database_url = 'postgres://odoo:odoo@localhost:8070/odoo'
+default_filestore_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'filestore', 'odoo')
+
 def restore_filestore(
   filestore_dir: str,
   odoo_filestore_dir: str,
 ):
   if not os.path.exists(filestore_dir):
     raise Exception(f'Filestore dir not found: {filestore_dir}')
-  if not os.path.exists(odoo_filestore_dir):
-    raise Exception(f'Odoo filestore dir not found: {odoo_filestore_dir}')
+  if os.path.exists(odoo_filestore_dir):
+    logger.debug(f'Removing existing filestore dir: {odoo_filestore_dir}')
+    shutil.rmtree(odoo_filestore_dir)
   
   logger.debug(f'Restoring filestore from {filestore_dir} to {odoo_filestore_dir}')
-  shutil.rmtree(odoo_filestore_dir)
+  if not os.path.exists(os.path.dirname(odoo_filestore_dir)):
+    os.makedirs(os.path.dirname(odoo_filestore_dir))
   shutil.copytree(filestore_dir, odoo_filestore_dir)
 
 
@@ -29,7 +34,6 @@ def restore_database(
     raise Exception(f'Dump file not found: {dump_path}')
   
   database_name = odoo_database_url.split('/')[-1]
-  # replace database name with postgres database name
   postgres_database_url = '/'.join(odoo_database_url.split('/')[:-1]) + '/postgres'
 
   logger.debug(f'Dropping database {database_name}')
@@ -45,8 +49,8 @@ def restore_database(
 def restore(
   name: str,
   snapshot_dir: str,
-  odoo_database_url: str,
-  odoo_filestore_dir: str,
+  odoo_database_url: str = default_database_url,
+  odoo_filestore_dir: str = default_filestore_dir,
 ):
   snapshot_path = os.path.join(snapshot_dir, f'{name}.zip')
 
@@ -57,14 +61,15 @@ def restore(
     
     restore_filestore(os.path.join(temp_dir, 'filestore'), odoo_filestore_dir)
     restore_database(os.path.join(temp_dir, 'dump.sql'), odoo_database_url)
+  
+  logger.info(f'Restored snapshot {name}')
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--name', type=str, required=True)
   parser.add_argument('--snapshot-dir', type=str, required=True)
-  parser.add_argument('--database-url', type=str, default='postgres://odoo:odoo@localhost:8070/odoo')
-  parser.add_argument('--odoo-filestore-dir', type=str, required=True)
+  parser.add_argument('--database-url', type=str, default=default_database_url)
   args = parser.parse_args()
 
   logging.basicConfig(level=logging.DEBUG)
@@ -73,5 +78,4 @@ if __name__ == '__main__':
     name=args.name,
     snapshot_dir=args.snapshot_dir,
     odoo_database_url=args.database_url,
-    odoo_filestore_dir=args.odoo_filestore_dir,
   )
